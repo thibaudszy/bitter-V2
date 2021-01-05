99c8e800c0e04edae32c3d9c75fca4ea53e1d34e
thibaudszy <thivaudszy@gmail.com>
2021-01-05 21:52:45 +0100

    adds user seeder

---
+++ seeders/20201124164535-some-users.js
@@ -0,0 +1,56 @@
+"use strict";
+const bcrypt = require("bcrypt");
+const { SALT_ROUNDS } = require("../config/constants");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          firstName: "Thibaud",
+          lastName: "Szymczak",
+          email: "thibaud@gmail.com",
+          password: bcrypt.hashSync("1234", SALT_ROUNDS),
+          language: "En-GB",
+          gender: "male",
+          preferedUnitMass: "metric",
+          preferedUnitBeerDensity: "plato",
+          generalBrewingUnits: "EBC",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          firstName: "Bobby",
+          lastName: "Bob",
+          email: "bobby@gmail.com",
+          password: bcrypt.hashSync("1234", SALT_ROUNDS),
+          language: "En-GB",
+          gender: "male",
+          preferedUnitMass: "imperial",
+          preferedUnitBeerDensity: "gravity",
+          generalBrewingUnits: "UK",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          firstName: "Emma",
+          lastName: "Lupulus",
+          email: "Emma@gmail.com",
+          password: bcrypt.hashSync("1234", SALT_ROUNDS),
+          language: "Fr-FR",
+          gender: "female",
+          preferedUnitMass: "plato",
+          preferedUnitBeerDensity: "gravity",
+          generalBrewingUnits: "UK",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
